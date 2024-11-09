ce1cf7616ebc44c32699dada2b02ab4ac2cbaa9f
aramrw <106574385+aramrw@users.noreply.github.com>
2024-11-09 11:16:56 -0800

    Create models.d.ts

---
+++ src/models.d.ts
@@ -0,0 +1,43 @@
+export type UserType = {
+  id: string;
+  username: string;
+  settings: SettingsType;
+}
+
+export type SettingsType = {
+  user_id: string;
+  mpv_path?: string;
+  plugins_path?: string;
+  autoplay: boolean;
+  update_date: string;
+  update_time: string;
+}
+
+export type UserFormType = {
+  username: string;
+}
+
+export type OsFolder = {
+  user_id: string;
+  path: string;
+  title: string;
+  parent_path: string | undefined;
+  last_read_panel: MangaPanel | undefined;
+  cover_img_path: string | undefined;
+  is_manga_folder: boolean;
+  is_double_panels: boolean;
+  zoom: number;
+  update_date: string;
+  update_time: string;
+}
+
+export type MangaPanel = {
+  user_id: string;
+  path: string;
+  title: string;
+  parent_path: string;
+  is_read: bool;
+  update_date: string;
+  update_time: string;
+}
+
