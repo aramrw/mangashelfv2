dcb1cc67f3f50c02e0094a43c9eb617692c0213a
aramrw <106574385+aramrw@users.noreply.github.com>
2024-11-13 14:49:23 -0800

    Update reader-nav.tsx

--- src/routes/reader/reader-nav.tsx
+++ src/routes/reader/reader-nav.tsx
@@ -1,97 +1,148 @@
-import { IconAdjustments, IconArrowNarrowLeftDashed, IconChalkboard, IconColumns2, IconDeviceDesktopAnalytics, IconList, IconMenu, IconZoomIn, IconZoomOut } from "@tabler/icons-solidjs";
-import {
-  Sheet,
-  SheetContent,
-  SheetTrigger
-} from "../../components/ui/sheet";
+import { IconAdjustments, IconArrowNarrowLeftDashed, IconColumns2, IconZoomIn, IconZoomOut } from "@tabler/icons-solidjs";
 import { A, useNavigate } from "@solidjs/router";
-import { Accessor, createSignal, onCleanup, onMount, Setter } from "solid-js";
+import { Accessor, createSignal, onCleanup, onMount, Resource, Setter } from "solid-js";
 import { MangaPanel, OsFolder, UserType } from "../../models";
-import { invoke } from "@tauri-apps/api/core";
 import { update_os_folders } from "../../tauri-cmds/os_folders";
-import { handleNextPanel, handleNextSinglPanel, handlePrevPanel, handlePrevSinglePanel } from "./reader";
 
 export default function ReaderNavbar({
   user,
   folder,
+  parentFolder,
   panels,
   panelIndex,
   isDoublePanels,
   currentZoomLevel,
   setZoom,
   setPanelIndex,
-  handleSetDoublePanels
+  setCurrentMangaFolder,
+  handleSetDoublePanels,
+  handleSetFirstPanel,
+  handleSetLastPanel,
+  handlePrevSinglePanel,
+  handlePrevPanel,
+  handleNextSinglePanel,
+  handleNextPanel,
 }: {
-  user: UserType;
-  folder: OsFolder;
-  panels: MangaPanel[];
+  user: Resource<UserType | null>;
+  folder: Resource<OsFolder | null>;
+  parentFolder: Resource<OsFolder | null>;
+  panels: Resource<MangaPanel[] | null>;
   panelIndex: Accessor<number>;
   isDoublePanels: Accessor<boolean>;
   currentZoomLevel: Accessor<number>;
   setZoom: Setter<number>;
   setPanelIndex: Setter<number>,
+  setCurrentMangaFolder: Setter<OsFolder | null | undefined>
   handleSetDoublePanels(): Promise<void>;
+  handleSetFirstPanel: () => Promise<void>;
+  handleSetLastPanel: () => Promise<void>;
+  handlePrevSinglePanel: () => Promise<void>;
+  handlePrevPanel: () => Promise<void>;
+  handleNextSinglePanel: () => Promise<void>;
+  handleNextPanel: () => Promise<void>;
 }) {
-
-  console.log(folder);
-
   const navigate = useNavigate();
 
   // Define refs for the intervals
   const [zoomInInterval, setZoomInInterval] = createSignal<number | undefined>(undefined);
   const [zoomOutInterval, setZoomOutInterval] = createSignal<number | undefined>(undefined);
 
-  const handleZoomStart = (
-    type: "magnify" | "minify",
-    setZoom: Setter<number>,
-  ) => {
-    handleZoomStop();  // Stop any ongoing zoom intervals before starting a new one
-    if (type === "magnify") {
-      setZoomInInterval(window.setInterval(() => {
-        setZoom((prev) => prev + 10);
-      }, 80));
-    } else {
-      setZoomOutInterval(window.setInterval(() => {
-        setZoom((prev) => prev - 10);
-      }, 80));
-    }
+  const handleZoomStart = (type: "magnify" | "minify", setZoom: Setter<number>) => {
+    // Stop any ongoing zoom intervals before starting a new one
+    handleZoomStop().then(() => {
+      const zoomFunction = (type === "magnify")
+        ? () => setZoom((prev) => Math.min(prev + 1, 900)) // Increase by 1, max zoom 200
+        : () => setZoom((prev) => Math.max(prev - 1, 1)); // Decrease by 1, min zoom 1
+
+      const zoomLoop = () => {
+        zoomFunction();
+        if (zoomInInterval()) {
+          requestAnimationFrame(zoomLoop); // Keep zooming as long as the interval is active
+        }
+      };
+
+      setZoomInInterval(() => requestAnimationFrame(zoomLoop)); // Start the animation loop
+    });
   };
 
   const handleZoomStop = async () => {
+    // Clear the zoom loop
     if (zoomInInterval() !== undefined) {
-      clearInterval(zoomInInterval()!);
+      cancelAnimationFrame(zoomInInterval()!);
       setZoomInInterval(undefined);
     }
     if (zoomOutInterval() !== undefined) {
-      clearInterval(zoomOutInterval()!);
+      cancelAnimationFrame(zoomOutInterval()!);
       setZoomOutInterval(undefined);
     }
-    let newFolder = structuredClone(folder);
-    newFolder.zoom = currentZoomLevel();
-    await update_os_folders([newFolder], user.id);
+    let newFolder = structuredClone(folder());
+    if (newFolder && user()?.id) {
+      newFolder.zoom = currentZoomLevel();
+      await update_os_folders([newFolder], user()!.id).then(() => {
+        setCurrentMangaFolder(newFolder);
+      });
+    }
   };
 
 
   const keyDownHandler = async (event: KeyboardEvent) => {
+    event.preventDefault();  // Prevent default behavior like page navigation on keydown
+    event.stopPropagation();  // Stop the event from propagating to other listeners 
     handleZoomStop()
     if (event.ctrlKey && event.key === "=") {
-      setZoom((prev) => prev + 10); // Zoom in by 10 units
+      setZoom((prev) => prev + 10);
     } else if (event.ctrlKey && event.key === "-") {
-      setZoom((prev) => prev - 10); // Zoom out by 10 units
+      setZoom((prev) => prev - 10);
     } else {
       // Other keybinds handling as before
       await handleKeyDown(
         event,
-        panels,
-        panelIndex(),
-        isDoublePanels(),
-        setPanelIndex,
-        setZoom,
-        handleSetDoublePanels,
-        handleZoomStart
       );
     }
   };
+
+
+  const handleKeyDown = async (
+    event: KeyboardEvent,
+  ) => {
+    // set first and last panel
+    if (event.ctrlKey && event.key === "ArrowRight") {
+      await handleSetFirstPanel();
+    } else if (event.ctrlKey && event.key === "ArrowLeft") {
+      await handleSetLastPanel();
+    }
+    // previous panels
+    else if (event.shiftKey && event.key === "ArrowRight") {
+      await handlePrevSinglePanel();
+    } else if (event.key === "ArrowRight") {
+      if (isDoublePanels()) {
+        await handlePrevPanel();
+      } else {
+        await handlePrevSinglePanel();
+      }
+    }
+    // next panels
+    else if (event.shiftKey && event.key === "ArrowLeft") {
+      await handleNextSinglePanel();
+    } else if (event.key === "ArrowLeft") {
+      if (isDoublePanels()) {
+        await handleNextPanel();
+      } else {
+        await handleNextSinglePanel();
+      }
+    }
+    // double panels
+    else if (event.key === "p") {
+      await handleSetDoublePanels();
+    }
+
+    // handle zoom level
+    if (event.ctrlKey && event.key === "=") {
+      handleZoomStart("magnify", setZoom);
+    } else if (event.ctrlKey && event.key === "-") {
+      handleZoomStart("minify", setZoom);
+    }
+  }
 
   onMount(() => {
     addEventListener("keydown", keyDownHandler);
@@ -113,15 +164,17 @@ export default function ReaderNavbar({
 
   return (
     <nav class="md:px-[115px] lg:px-[148px] xl:px-[200px] w-full h-8 bg-primary shadow-md z-[100]">
-      <ul class="h-full w-full flex flex-row items-center justify-between">
+      <ul class="h-full w-full flex flex-row items-center justify-between relative">
         <div class="w-fit flex flex-row h-full">
-          <li class="px-1 h-full flex flex-row justify-center items-center hover:bg-accent transition-colors cursor-pointer"
-            onClick={() => navigate(-1)}
+          <li
+            class="px-1 h-full flex flex-row justify-center items-center hover:bg-accent transition-colors cursor-pointer"
+            onClick={() => {
+              navigate(-1);
+            }}
           >
             <IconArrowNarrowLeftDashed class="text-secondary fill-accent stroke-[2]" />
           </li>
-          <li class="p-1 w-5 h-full flex flex-row justify-center items-center">
-          </li>
+          <li class="p-1 w-5 h-full flex flex-row justify-center items-center"></li>
           <li
             class="px-1 h-full flex flex-row justify-center items-center hover:bg-accent transition-colors cursor-pointer"
             onClick={async () => await handleSetDoublePanels()}
@@ -131,20 +184,21 @@ export default function ReaderNavbar({
           <li
             class="px-1 h-full flex flex-row justify-center items-center hover:bg-accent transition-colors cursor-pointer"
             onMouseDown={() => handleZoomStart("minify", setZoom)}
-            onMouseUp={() => handleZoomStop()}
-            onMouseLeave={() => handleZoomStop()}
+            onMouseUp={async () => await handleZoomStop()}
+            onMouseLeave={async () => await handleZoomStop()}
           >
             <IconZoomOut class="text-secondary fill-accent stroke-[1.5]" />
           </li>
           <li
             class="px-1 h-full flex flex-row justify-center items-center hover:bg-accent transition-colors cursor-pointer"
             onMouseDown={() => handleZoomStart("magnify", setZoom)}
-            onMouseUp={() => handleZoomStop()}
-            onMouseLeave={() => handleZoomStop()}
+            onMouseUp={async () => await handleZoomStop()}
+            onMouseLeave={async () => await handleZoomStop()}
           >
             <IconZoomIn class="text-secondary fill-accent stroke-[1.5]" />
           </li>
         </div>
+
         <li class="px-1 h-full flex flex-row justify-center items-center hover:bg-accent transition-colors cursor-pointer">
           <A href="/settings/default">
             <IconAdjustments class="text-secondary fill-accent stroke-[1.5]" />
@@ -154,55 +208,4 @@ export default function ReaderNavbar({
     </nav>
   );
 }
-
-const handleKeyDown = async (
-  event: KeyboardEvent,
-  panels: MangaPanel[],
-  panelIndex: number,
-  isDoublePanels: boolean,
-  setPanelIndex: Setter<number>,
-  setZoom: Setter<number>,
-  handleSetDoublePanels: () => Promise<void>,
-  handleZoomStart: (type: "magnify" | "minify", setZoom: Setter<number>) => void
-) => {
-  // set first and last panel
-  if (event.ctrlKey && event.key === "ArrowRight") {
-    //handleSetFirstPanel();
-  } else if (event.ctrlKey && event.key === "ArrowLeft") {
-    //handleSetLastPanel();
-  }
-  // previous panels
-  else if (event.shiftKey && event.key === "ArrowRight") {
-    handlePrevSinglePanel(panelIndex, setPanelIndex);
-  } else if (event.key === "ArrowRight") {
-    if (isDoublePanels) {
-      handlePrevPanel(panelIndex, setPanelIndex);
-    } else {
-      handlePrevSinglePanel(panelIndex, setPanelIndex);
-    }
-  }
-  // next panels
-  else if (event.shiftKey && event.key === "ArrowLeft") {
-    handleNextSinglPanel(panels, panelIndex, setPanelIndex);
-  } else if (event.key === "ArrowLeft") {
-    if (isDoublePanels) {
-      handleNextPanel(panels, panelIndex, setPanelIndex);
-    } else {
-      handleNextSinglPanel(panels, panelIndex, setPanelIndex);
-    }
-  }
-  // double panels
-  else if (event.key === "p") {
-    await handleSetDoublePanels();
-  }
-
-  // handle zoom level
-  if (event.ctrlKey && event.key === "=") {
-    handleZoomStart("magnify", setZoom);
-  } else if (event.ctrlKey && event.key === "-") {
-    handleZoomStart("minify", setZoom);
-  }
-}
-
-
 
