ea8bc6046efc63334243067b2c5ee1e2b43bc86a
aramrw <106574385+aramrw@users.noreply.github.com>
2024-11-21 19:06:21 -0800

    Update dashboard.tsx

--- src/dashboard/dashboard.tsx
+++ src/dashboard/dashboard.tsx
@@ -11,21 +11,36 @@ export default function Dashboard() {
   const [osFolders, { refetch }] = createResource<OsFolder[]>(async () => {
     const user = await get_default_user();
     if (user) {
+			console.log(user);
       setUser(user);
       const folders = await get_os_folders(user.id);
-      console.log(folders);
+      console.log("dashboard folders: ", folders);
       return folders || []; // Always return an array, even if empty
     }
     return []; // Return an empty array if no user is found
   })
+  const [showHiddenFolders, setShowHiddenFolders] = createSignal(false);
 
   return (
     <main>
-      <NavBar />
+      <NavBar
+        showHiddenFolders={showHiddenFolders}
+        setShowHiddenFolders={setShowHiddenFolders}
+      />
       <section class="h-fit w-full py-4 px-3 md:px-16 lg:px-28 xl:px-44 flex flex-row gap-2">
         <div class="grid grid-cols-4 gap-2 min-h-[calc(4*cardHeight)]">
           <Show when={user() && osFolders.state === "ready" && osFolders()!.length > 0}>
-            <For each={osFolders()}>{(folder) => <OsFolderCard folder={folder} user={user} refetch={refetch} />}</For>
+            <For each={osFolders()}>{(folder) =>
+              <>
+                <Show when={showHiddenFolders() && folder.is_hidden}>
+                  <OsFolderCard folder={folder} user={user} refetch={refetch} />
+                </Show>
+                <Show when={!folder.is_hidden}>
+                  <OsFolderCard folder={folder} user={user} refetch={refetch} />
+                </Show>
+              </>
+            }
+            </For>
           </Show>
           <AddNewSkeleton user={user} refetch={refetch} />
         </div>
