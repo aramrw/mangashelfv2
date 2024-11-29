ff97301356c161836a121c8dd221151a2d07c951
aramrw <106574385+aramrw@users.noreply.github.com>
2024-11-28 17:26:33 -0800

    Update dashboard.tsx

--- src/dashboard/dashboard.tsx
+++ src/dashboard/dashboard.tsx
@@ -2,44 +2,41 @@ import { createResource, createSignal, For, Show } from "solid-js";
 import NavBar from "../main-components/navbar";
 import { OsFolder, UserType } from "../models";
 import AddNewSkeleton from "./components/add-new-skeleton";
-import OsFolderCard from "./components/os-folder-card";
 import { get_os_folders } from "../tauri-cmds/os_folders";
 import get_default_user from "../tauri-cmds/users";
+import OsFolderCard from "./components/os-folder-card";
 
 export default function Dashboard() {
   const [user, setUser] = createSignal<UserType | null>(null);
   const [osFolders, { refetch }] = createResource<OsFolder[]>(async () => {
     const user = await get_default_user();
     if (user) {
-			console.log(user);
       setUser(user);
       const folders = await get_os_folders(user.id);
       console.log("dashboard folders: ", folders);
       return folders || []; // Always return an array, even if empty
     }
     return []; // Return an empty array if no user is found
-  })
+  });
   const [showHiddenFolders, setShowHiddenFolders] = createSignal(false);
 
   return (
     <main>
-      <NavBar
-        showHiddenFolders={showHiddenFolders}
-        setShowHiddenFolders={setShowHiddenFolders}
-      />
+      <NavBar showHiddenFolders={showHiddenFolders} setShowHiddenFolders={setShowHiddenFolders} />
       <section class="h-fit w-full py-4 px-3 md:px-16 lg:px-28 xl:px-44 flex flex-row gap-2">
         <div class="grid grid-cols-4 gap-2">
           <Show when={user() && osFolders.state === "ready" && osFolders()!.length > 0}>
-            <For each={osFolders()}>{(folder) =>
-              <>
-                <Show when={showHiddenFolders() && folder.is_hidden}>
-                  <OsFolderCard folder={folder} user={user} refetch={refetch} />
-                </Show>
-                <Show when={!folder.is_hidden}>
-                  <OsFolderCard folder={folder} user={user} refetch={refetch} />
-                </Show>
-              </>
-            }
+            <For each={osFolders()}>
+              {(folder) => (
+                <>
+                  <Show when={showHiddenFolders() && folder.is_hidden}>
+                    <OsFolderCard folder={folder} user={user} refetch={refetch} />
+                  </Show>
+                  <Show when={!folder.is_hidden}>
+                    <OsFolderCard folder={folder} user={user} refetch={refetch} />
+                  </Show>
+                </>
+              )}
             </For>
           </Show>
           <AddNewSkeleton user={user} refetch={refetch} />
