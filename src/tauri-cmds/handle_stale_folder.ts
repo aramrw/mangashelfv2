5559bb932e9f7afdb6b907cd1abc1d405d2207cd
aramrw <106574385+aramrw@users.noreply.github.com>
2024-12-12 10:09:45 -0800

    Update handle_stale_folder.ts

--- src/tauri-cmds/handle_stale_folder.ts
+++ src/tauri-cmds/handle_stale_folder.ts
@@ -1,16 +1,16 @@
 import { invoke } from "@tauri-apps/api/core";
-import { MangaPanel, OsFolder } from "../models";
+import { MangaPanel, OsFolder, UserType } from "../models";
 
 export default async function upsert_read_os_dir(
   dir: String,
   parentPath: String | undefined,
-  userId: String,
+  user: UserType,
   oldDirs: OsFolder[] | undefined,
   oldPanels: MangaPanel[] | undefined
 ) {
   try {
-		console.log("upsert_read_os_dir:", dir);
-    return await invoke("upsert_read_os_dir", { dir, parentPath, userId, oldDirs, oldPanels }) as boolean;
+    //console.log("upsert_read_os_dir:", dir);
+    return await invoke("upsert_read_os_dir", { dir, parentPath, user, oldDirs, oldPanels }) as boolean;
   } catch (e) {
     console.error("upsert_read_os_dir: ", e);
   }
